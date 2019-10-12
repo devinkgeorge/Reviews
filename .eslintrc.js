7c91dde61683ecba26f0c21ee2ca840117c00b93
Devin George <devin.k.george@gmail.com>
2019-10-11 19:47:41 -0500

    Add sample jest test & config eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+  extends: ['airbnb', 'prettier', 'plugin:node/recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'no-console': 'warn',
+    'prettier/prettier': 'error'
+  }
 };
---
+++ tests/app.test.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+test('This is a fake test', () => {
+  expect(true).toBeTruthy();
+});
