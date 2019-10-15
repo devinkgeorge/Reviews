d495bcbc891f94a2888b3f49f53a78aa01db34d6
Devin George <devin.k.george@gmail.com>
2019-10-15 16:22:03 -0500

    Commit before initial deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['airbnb', 'prettier', 'plugin:node/recommended'],
   plugins: ['prettier'],
   rules: {
-    'no-console': 'warn',
+    'no-console': 'off',
     'prettier/prettier': 'error'
   }
 };
--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
-//Dependencies
-const express = require('express')
-const http = require('http')
-const path = require('path')
+// Dependencies
+const express = require('express');
+// const http = require('http');
+const path = require('path');
 
-//Instantation
-const app = express()
-const port = 3001
+// Instantation
+const app = express();
+const port = 3001;
 
-//Middleware
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, '/../public')))
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/../public')));
 
-//Routes
+// Routes
 app.get('/reviews', (req, res) => {
-  res.send('Blank reviews GET request working')
-})
+  res.send('Blank reviews GET request working');
+});
 
 app.post('/reviews', (req, res) => {
-  res.send('Blank reviews POST request working')
-})
+  res.send('Blank reviews POST request working');
+});
 
 app.put('/reviews', (req, res) => {
-  res.send('Blank reviews PUT request working')
-})
+  res.send('Blank reviews PUT request working');
+});
 
-//Listen
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+// Listen
+app.listen(port, () => console.log(`Listening on port ${port}!`));
