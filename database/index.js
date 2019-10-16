aeffd01896009856c4e05f930d4001bde55921a6
Devin George <devin.k.george@gmail.com>
2019-10-16 18:22:11 -0500

    Add connections to Atlas db

--- database/index.js
+++ database/index.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
-
-// Set up default mongoose connection
-const mongoDB = 'mongodb://127.0.0.1/Amishon';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+const Review = require('./models/reviews');
+const reviews = require('./data.js');
 
 // Get the default connection
 const db = mongoose.connection;
 
 //  Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('db connected!');
+});
+
+// Set up mongoose connection to Atlas
+const mongoAtlas =
+  'mongodb+srv://dkgeorge:AE2PX9NAABSEJ6JU@amishon-reviews-ajtnd.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoAtlas, { useNewUrlParser: true });
+
+Review.insertMany(reviews, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('successfully seeded');
+  }
+});
