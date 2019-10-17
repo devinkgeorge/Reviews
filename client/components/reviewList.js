9c874e17941206cfd2a820fce112e726385541c0
Devin George <devin.k.george@gmail.com>
2019-10-17 15:48:58 -0500

    Add layout for the review list comp

--- client/components/reviewList.js
+++ client/components/reviewList.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import Review from 'review.js'
+import Review from './review.js'
 
 const ReviewList = (props) => {
   return (
     <div>
-      {
-        props.currentProductReviews.map( currentReview => {
-          return (
-            <Review review={currentReview} />
-          )
-        })
-      }
+      <h5 className="number-reviews-header">{props.currentProductReviews.length} customer reviews</h5>
+      <div>This is where the dropdown to sort reviews will be</div>
+      <div>
+        {
+          props.currentProductReviews.map( currentReview => {
+            return (
+              <Review review={currentReview} />
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
