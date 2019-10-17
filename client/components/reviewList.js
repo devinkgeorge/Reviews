a6e41129b5c7f83b73e5686d4f81f53dcfcb90ef
Devin George <devin.k.george@gmail.com>
2019-10-17 18:24:42 -0500

    Add write review button

--- client/components/reviewList.js
+++ client/components/reviewList.js
@@ -4,17 +4,23 @@ import Review from './review.js'
 const ReviewList = (props) => {
   return (
     <div>
-      <h5 className="number-reviews-header">{props.currentProductReviews.length} customer reviews</h5>
+      <h5 className="number-reviews-header">{props.currentProductReviews.length} Amish reviews</h5>
       <div>This is where the dropdown to sort reviews will be</div>
       <div>
         {
           props.currentProductReviews.map( currentReview => {
             return (
-              <Review review={currentReview} />
+              <Review review={currentReview} key={currentReview.review_id} />
             )
           })
         }
       </div>
+      <div className="see-all-reviews-div">
+        <a href="URLforAllReviewsPage" className="see-all-reviews">
+          See all {props.currentProductReviews.length} Amish reviews
+        </a>
+      </div>
+      <button className="write-review-button">Write an Amish review</button>
     </div>
   )
 }
