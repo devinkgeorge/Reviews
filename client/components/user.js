8249c3a0f23431ee2c541adeae5dc5128feb765b
Devin George <devin.k.george@gmail.com>
2019-10-17 16:06:39 -0500

    Fix typo in class name

--- client/components/user.js
+++ client/components/user.js
@@ -4,7 +4,7 @@ const User = props => {
   return (
     <div>
       <img className="user-thumbnail" src="https://images-na.ssl-images-amazon.com/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"></img>
-      <span className="reviewr-name">This is the username for user id {props.user}</span>
+      <span className="reviewer-name">This is the username for user id {props.user}</span>
     </div>
   )
 }
