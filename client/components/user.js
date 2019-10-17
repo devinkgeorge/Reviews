dbdf3742b97bce366130c28a6963535f12b458af
Devin George <devin.k.george@gmail.com>
2019-10-17 15:46:05 -0500

    Add user component for reviews

---
+++ client/components/user.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const User = props => {
+  return (
+    <div>
+      <img className="user-thumbnail" src="https://images-na.ssl-images-amazon.com/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"></img>
+      <span className="reviewr-name">This is the username for user id {props.user}</span>
+    </div>
+  )
+}
+
+export default User;
