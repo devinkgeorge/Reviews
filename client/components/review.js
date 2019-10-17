91b56323fb8d6977d52198551ebb7e0905b8d241
Devin George <devin.k.george@gmail.com>
2019-10-17 17:08:13 -0500

    Add date stamp and info components

--- client/components/review.js
+++ client/components/review.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import User from './user.js'
-import ReviewTitle from './reviewTitle.js'
+import User from './user.js';
+import ReviewTitle from './reviewTitle.js';
+import DateStamp from './dateStamp.js';
+import ItemInfo from './itemInfo.js';
 
 const Review = props => {
   return (
     <div>
       <User user={props.review.user_id} />
       <ReviewTitle rating={props.review.stars} title={props.review.title} />
+      <DateStamp date={props.review.created} />
+      <ItemInfo
+        style={props.review.style}
+        size={props.review.size}
+        color={props.review.color}
+        verified={props.review.verified}
+      />
     </div>
   )
 }
