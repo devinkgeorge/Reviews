4fa58a9f7186a96e611c9004e8f97b66a402bffd
Devin George <devin.k.george@gmail.com>
2019-10-17 15:13:40 -0500

    Refactor and import ReviewList

--- client/app.js
+++ client/app.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReviewList from './components/reviewList.js'
 
 class Reviews extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentProduct: 13
+      currentProduct: 13,
+      currentProductReviews: [
+        {
+          review_id: 13,
+          product_id: 13,
+          created: 20131313,
+          title: 'Manure',
+          user_id: 13,
+          body: 'Radish on kidney beans',
+          verified: true,
+          stars: 5,
+          style: '',
+          helpfuls: 13
+        }
+      ]
     }
+    this.updateProduct = this.updateProduct.bind(this);
   }
 
   updateProduct (newProduct) {
-    this.setState({currentProduct: newProduct})
+    this.setState({ currentProduct: newProduct })
   }
-
-
 
   render () {
     return (
       <div>
-        This is the React test div
+        <ReviewList currentProductReviews={this.state.currentProductReviews} />
       </div>
     )
   }
@@ -26,7 +40,4 @@ class Reviews extends React.Component {
 
 
 // export default Reviews;
-ReactDOM.render(
-  React.createElement(Reviews),
-  document.getElementById('reviews')
-);
+ReactDOM.render(<Reviews />, document.getElementById('reviews'));
