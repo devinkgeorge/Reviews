2934d7b34d61f685c61438d4fc9f644d210de41b
Devin George <devin.k.george@gmail.com>
2019-10-17 16:07:29 -0500

    Add user and title components

--- client/components/review.js
+++ client/components/review.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import User from './user.js'
+import ReviewTitle from './reviewTitle.js'
 
-const Review = (props) => {
+const Review = props => {
   return (
-
+    <div>
+      <User user={props.review.user_id} />
+      <ReviewTitle rating={props.review.stars} title={props.review.title} />
+    </div>
   )
 }
 
