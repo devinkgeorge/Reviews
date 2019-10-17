b99e76a725005908e0098aa4cdf506629deb7dc3
Devin George <devin.k.george@gmail.com>
2019-10-16 19:18:04 -0500

    Add initial layout for react comps

--- client/app.js
+++ client/app.js
@@ -1,13 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Reviews extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      currentProduct: 13
     }
   }
+
+  updateProduct (newProduct) {
+    this.setState({currentProduct: newProduct})
+  }
+
+
 
   render () {
     return (
---
+++ client/components/reviewList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Review from 'review.js'
+
+const ReviewList = (props) => {
+  return (
+
+  )
+}
+
+export default ReviewList;
