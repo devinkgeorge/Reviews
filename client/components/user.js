34fb2c998ccf37bf070a1a764031d5b6a12b4f58
Devin George <devin.k.george@gmail.com>
2019-10-18 18:01:04 -0500

    Refactor for styling

--- client/components/user.js
+++ client/components/user.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const User = props => {
   return (
-    <div>
-      <img className="user-thumbnail" src={props.user.userimg}></img>
-      <span className="reviewer-name">{props.user.username}</span>
+    <div className="reviewer-container">
+      <div className="reviewer-thumbnail-wrapper">
+        <img
+          className="reviewer-thumbnail"
+          src={props.user.userimg}
+        >
+        </img>
+      </div>
+      <div className="reviewer-name-wrapper">
+        <span className="reviewer-name">
+          {props.user.username}
+        </span>
+      </div>
     </div>
   )
 }
