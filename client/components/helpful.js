4ddf02591abbeb0038a4832287231c52c34c9420
Devin George <devin.k.george@gmail.com>
2019-10-18 23:13:52 -0500

    Add styling to review helpful bar

--- client/components/helpful.js
+++ client/components/helpful.js
@@ -3,12 +3,18 @@ import React from 'react';
 const Helpful = props => {
   return (
     <div>
-      { props.helpfuls > 0 ?
-        <div className="helpful-number">{props.helpfuls} Amish found this helpful</div>
-        :
-        null
+      { props.helpfuls > 0 &&
+        <div className="helpful-number">
+          {props.helpfuls} Amish found this helpful
+        </div>
       }
-      <button className="helpful-button">Helpful</button>
+      <div className="helpful-button-container">
+        <span className="helpful-button">
+          <span className="helpful-button-inner">
+            Helpful
+          </span>
+        </span>
+      </div>
       <a href="commentsPageUrl" className="review-comment-link">Comment</a>
       <a href="reportAbusePageUrl" className="report-abuse-link">Report abuse</a>
     </div>
